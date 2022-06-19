03cbcf45bf19f6a4317797188b1f588deb5b2cb4
jesusquev3311 <jesusquev3311@gmail.com>
2022-06-18 22:17:02 -0400

    handling 404

--- section5/app.js
+++ section5/app.js
@@ -13,6 +13,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next)=>{
+    res.status(404).send("<h1>404 Not Found</h1>");
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);

12785e0c30386b849cdf8b01b9b011d95e70c88d
jesusquev3311 <jesusquev3311@gmail.com>
2022-06-18 22:03:12 -0400

    adding controllers for routes

--- section5/app.js
+++ section5/app.js
@@ -4,23 +4,15 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use("/add-product", (req, res, next)=>{
-    console.log("I'm another middleware");
-    res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>send</button></form>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
-});
-
-app.use("/", (req, res, next)=>{
-    console.log("I'malways running");
-    res.send("<h1>Home Pae</h1>");
-});
 const server = http.createServer(app);
 
 server.listen(3000);

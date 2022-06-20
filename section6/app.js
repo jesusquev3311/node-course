0e4e280f46254d490d935dff29009d9490167e4a
jesusquev3311 <jesusquev3311@gmail.com>
2022-06-19 21:33:50 -0400

    adding section 6

---
+++ section6/app.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "css")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+})
+
+const server = http.createServer(app);
+
+server.listen(3000);
---
+++ section6/routes/shop.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+const express = require("express");
+const { products } = require("./admin");
+
+const router = express.Router();
+
+router.get("/", (req, res, next)=>{
+    console.log(products)
+    res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+});
+
+module.exports = router;
